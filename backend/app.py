1c723b338d48b7467326fe978738eaf039c29f4f
John Shajan <147299005+jxhn13@users.noreply.github.com>
2025-07-08 18:34:09 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS  # ✅ Import CORS
 from api.upload import upload_bp
 from api.chat import chat_bp
-
+import os
 app = Flask(__name__)
 CORS(app, origins=["https://memorygpt.vercel.app"])
 
@@ -14,5 +14,9 @@ app.register_blueprint(chat_bp, url_prefix="/api")
 def index():
     return {"message": "MemoryGPT Backend is running"}
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
